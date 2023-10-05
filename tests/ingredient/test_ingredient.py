e92c3cb8102fcb0eed4d10684948332524db34a2
Mateus Bertazzo <mateusbataglin52@gmail.com>
2023-10-05 14:23:57 -0300

    implementa testes da classe ingredients e resolve req 1

--- tests/ingredient/test_ingredient.py
+++ tests/ingredient/test_ingredient.py
@@ -1,6 +1,27 @@
-from src.models.ingredient import Ingredient  # noqa: F401, E261, E501
+from src.models.ingredient import (
+    Ingredient,
+    Restriction,
+)
 
 
 # Req 1
 def test_ingredient():
-    pass
+    ingredientTomato = Ingredient("tomato")
+    ingredientTomato2 = Ingredient("tomato")
+    assert ingredientTomato.name == "tomato"
+    assert hash(ingredientTomato) == hash(Ingredient("tomato"))
+    assert ingredientTomato.__repr__() == "Ingredient('tomato')"
+
+    ingredientMozzarella = Ingredient("queijo mussarela")
+    assert ingredientMozzarella.name == "queijo mussarela"
+    assert hash(ingredientMozzarella) == hash(Ingredient("queijo mussarela"))
+    assert ingredientMozzarella.__repr__() == "Ingredient('queijo mussarela')"
+
+    assert hash(ingredientTomato) != hash(ingredientMozzarella)
+    assert ingredientTomato != ingredientMozzarella
+    assert ingredientTomato == ingredientTomato2
+
+    assert ingredientMozzarella.restrictions == {
+        Restriction.LACTOSE,
+        Restriction.ANIMAL_DERIVED,
+    }
