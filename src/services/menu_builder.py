05d408d7092d68125e64592a62eaf26db600cbc9
Mateus Bertazzo <mateusbataglin52@gmail.com>
2023-10-05 17:39:31 -0300

    implementa get_main_menu que retorna o menu do restaurante baseado nos ingredientes disponiveis resolve req 4 e 6

--- src/services/menu_builder.py
+++ src/services/menu_builder.py
@@ -1,7 +1,7 @@
-from typing import Dict, List
-
 from services.inventory_control import InventoryMapping
 from services.menu_data import MenuData
+import pandas as pd
+
 
 DATA_PATH = "data/menu_base_data.csv"
 INVENTORY_PATH = "data/inventory_base_data.csv"
@@ -24,6 +24,20 @@ class MenuBuilder:
 
         self.inventory.consume_recipe(curr_dish.recipe)
 
-    # Req 4
-    def get_main_menu(self, restriction=None) -> List[Dict]:
-        pass
+    def get_main_menu(self, restriction=None) -> pd.DataFrame:
+        menu_list = [
+            {
+                "dish_name": dish.name,
+                "price": dish.price,
+                "ingredients": dish.get_ingredients(),
+                "restrictions": dish.get_restrictions(),
+            }
+            for dish in self.menu_data.dishes
+            if (
+                (restriction is None or restriction not in
+                 dish.get_restrictions()) and
+                all(ingredient in self.inventory.inventory for ingredient in
+                    dish.get_ingredients()))
+        ]
+
+        return menu_list
